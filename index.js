1aaf33afbd5c76c1a15440f3ff824a991d96a8db
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-28 05:11:26 +0000

    feat: add renderTeamMembers function

--- index.js
+++ index.js
@@ -1,75 +1,35 @@
 import fs from "fs";
 import inquirer from "inquirer";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import Manager from "./lib/Manager.js";
 import Engineer from "./lib/Engineer.js";
 import Intern from "./lib/Intern.js";
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+import render from "./src/page-template.js";
 
-// const render = require("./src/page-template.js");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// * Create a command-line application that accepts accepts user input using the provided starter code.
-//   * Create classes for each team member provided and export them. The tests for these classes (in the `_tests_` directory) must ALL pass.
-//     * The first class is an `Employee` parent class with the following properties and methods:
-//       * `name`
-//       * `id`
-//       * `email`
-//       * `getName()`
-//       * `getId()`
-//       * `getEmail()`
-//       * `getRole()`&mdash;returns `'Employee'`
-//     * The other three classes will extend `Employee`.
-//     * In addition to `Employee`'s properties and methods, `Manager` will also have the following:
-//       * `officeNumber`
-//       * `getRole()`&mdash;overridden to return `'Manager'`
-//     * In addition to `Employee`'s properties and methods, `Engineer` will also have the following:
-//       * `github`&mdash;GitHub username
-//       * `getGithub()`
-//       * `getRole()`&mdash;overridden to return `'Engineer'`
-//     * In addition to `Employee`'s properties and methods, `Intern` will also have the following:
-//       * `school`
-//       * `getSchool()`
-//       * `getRole()`&mdash;overridden to return `'Intern'`
-
-//     * Finally, although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
-
-//   * Write code in `index.js` that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
-//     * When a user starts the application then they are prompted to enter the **team manager**’s:
-//       * Name
-//       * Employee ID
-//       * Email address
-//       * Office number
-//     * When a user enters those requirements then the user is presented with a menu with the option to:
-//       * Add an engineer
-//       * Add an intern
-//       * Finish building the team
-//     * When a user selects the **engineer** option then a user is prompted to enter the following and then the user is taken back to the menu:
-//       * Engineer's Name
-//       * ID
-//       * Email
-//       * GitHub username
-//     * When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
-//       * Intern’s name
-//       * ID
-//       * Email
-//       * School
-//     * When a user decides to finish building their team then they exit the application, and the HTML is generated.
-//   * Call the `render` function (provided for you) and pass in an array containing all employee objects;
-//     * The `render` function will generate and return a block of HTML including templated divs for each employee!
-//   * Create an HTML file using the HTML returned from the `render` function.
-//     * Write it to a file named `team.html` in the `output` folder.
-//     * You can use the provided variable `outputPath` to target this location.
-
 const teamMembers = [];
-const idArray = [];
 
-// All common questions
-const commonQuestions = [
+const renderTeamMembers = () => {
+  return fs.writeFileSync(outputPath, render(teamMembers));
+};
+
+/**
+ * the user is prompted to enter the **team manager**’s: name, ID, and email address
+ * if the name is empty, the user is prompted to enter at least one character
+ * if the ID is empty, the user is prompted to enter a positive number greater than zero
+ * if the email is empty, the user is prompted to enter a valid email address
+ */
+const employeeQuestions = [
   {
     type: "input",
     name: "name",
@@ -116,6 +76,12 @@ const commonQuestions = [
   },
 ];
 
+/**
+ * after the initialization and the user answered all employee questions,
+ * the user is prompted to select one of the following options from **selectTeamMemberQuestion**:
+ * if the selected answer is **Add an engineer** or **Add an intern**, the user is prompted to enter additional information's
+ * else if the selected answer is **Finish building the team**, the user is prompted to exit the application
+ */
 const selectTeamMemberQuestion = [
   {
     type: "list",
@@ -125,8 +91,14 @@ const selectTeamMemberQuestion = [
   },
 ];
 
+/**
+ * at initialization, after the user answered employee questions
+ * the user is prompted to enter the **team manager**’s: office number
+ * if the office number is empty, the user is prompted to enter a positive number greater than zero
+ * after the user answered all manager questions, the user is prompted to select one of the following options from **selectTeamMemberQuestion**
+ */
 const managerQuestions = [
-  ...commonQuestions,
+  ...employeeQuestions,
   {
     type: "input",
     name: "officeNumber",
@@ -145,8 +117,17 @@ const managerQuestions = [
   ...selectTeamMemberQuestion,
 ];
 
+/**
+ * after the user selected **Add an engineer** from **selectTeamMemberQuestion**
+ * the user is prompted to enter the following: name, ID, email address, and GitHub username
+ * if the name is empty, the user is prompted to enter at least one character
+ * if the ID is empty, the user is prompted to enter a positive number greater than zero
+ * if the email is empty, the user is prompted to enter a valid email address
+ * if the GitHub username is empty, the user is prompted to enter at least one character
+ * after the user answered all engineer questions, the user is prompted to select if he would like to add another team member
+ */
 const engineerQuestions = [
-  ...commonQuestions,
+  ...employeeQuestions,
   {
     type: "input",
     name: "github",
@@ -166,8 +147,17 @@ const engineerQuestions = [
   },
 ];
 
+/**
+ * after the user selected **Add an intern** from **selectTeamMemberQuestion**
+ * the user is prompted to enter the following: name, ID, email address, and school
+ * if the name is empty, the user is prompted to enter at least one character
+ * if the ID is empty, the user is prompted to enter a positive number greater than zero
+ * if the email is empty, the user is prompted to enter a valid email address
+ * if the school is empty, the user is prompted to enter at least one character
+ * after the user answered all intern questions, the user is prompted to select if he would like to add another team member
+ */
 const internQuestions = [
-  ...commonQuestions,
+  ...employeeQuestions,
   {
     type: "input",
     name: "school",
@@ -187,6 +177,11 @@ const internQuestions = [
   },
 ];
 
+/**
+ * after and engineer or an intern is added to the team, the user is prompted to select if he would like to add another team member
+ * if the selected answer is **Add an engineer** or **Add an intern**, the user is prompted to enter additional information's
+ * else if the selected answer is **Finish building the team**, the user is prompted to exit the application
+ */
 const choseWhatRoleToAdd = () => {
   inquirer.prompt(selectTeamMemberQuestion).then((answers) => {
     const { moreMembers } = answers;
@@ -199,57 +194,63 @@ const choseWhatRoleToAdd = () => {
         addIntern();
         break;
       default:
+        renderTeamMembers();
         console.log("No more team members to add.");
     }
   });
 };
 
+/**
+ * add a new engineer to the **teamMembers** array
+ * prompt the user to select if he would like to add another team member
+ */
 const addEngineer = () => {
   inquirer.prompt(engineerQuestions).then((answers) => {
     const { name, id, email, github, moreMembers } = answers;
     const engineer = new Engineer(name, id, email, github);
     teamMembers.push(engineer);
-    idArray.push(id);
-
-    console.log(teamMembers);
 
     switch (moreMembers) {
       case "Yes":
         choseWhatRoleToAdd();
         break;
       default:
+        renderTeamMembers();
         console.log("No more team members to add.");
     }
   });
 };
 
+/**
+ * add a new intern to the **teamMembers** array
+ * prompt the user to select if he would like to add another team member
+ */
 const addIntern = () => {
   inquirer.prompt(internQuestions).then((answers) => {
     const { name, id, email, school, moreMembers } = answers;
     const intern = new Intern(name, id, email, school);
     teamMembers.push(intern);
-    idArray.push(id);
-
-    console.log(teamMembers);
 
     switch (moreMembers) {
       case "Yes":
         choseWhatRoleToAdd();
         break;
       default:
+        renderTeamMembers();
         console.log("No more team members to add.");
     }
   });
 };
 
+/**
+ * add the manager to the **teamMembers** array
+ * prompt the user to select if he would like to add another team member
+ */
 const addManager = () => {
   inquirer.prompt(managerQuestions).then((answers) => {
     const { name, id, email, officeNumber, moreMembers } = answers;
     const manager = new Manager(name, id, email, officeNumber);
     teamMembers.push(manager);
-    idArray.push(id);
-
-    console.log(teamMembers);
 
     switch (moreMembers) {
       case "Add an engineer":
@@ -259,6 +260,7 @@ const addManager = () => {
         addIntern();
         break;
       default:
+        renderTeamMembers();
         console.log("No more team members to add.");
     }
   });
--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,8 @@
 // creates the team
-const generateTeam = team => {
-
-    // creates the manager html
-    const generateManager = manager => {
-        return `
+const generateTeam = (team) => {
+  // creates the manager html
+  const generateManager = (manager) => {
+    return `
         <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
@@ -18,11 +17,11 @@ const generateTeam = team => {
         </div>
     </div>
         `;
-    };
+  };
 
-    // creates the html for engineers
-    const generateEngineer = engineer => {
-        return `
+  // creates the html for engineers
+  const generateEngineer = (engineer) => {
+    return `
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
@@ -37,11 +36,11 @@ const generateTeam = team => {
     </div>
 </div>
         `;
-    };
+  };
 
-    // creates the html for interns
-    const generateIntern = intern => {
-        return `
+  // creates the html for interns
+  const generateIntern = (intern) => {
+    return `
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
@@ -56,33 +55,34 @@ const generateTeam = team => {
     </div>
 </div>
         `;
-    };
+  };
 
-    const html = [];
+  const html = [];
 
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
 
-    return html.join("");
-
-}
+  return html.join("");
+};
 
 // exports function to generate entire page
-module.exports = team => {
-
-    return `
+const render = (team) => {
+  return `
     <!DOCTYPE html>
 <html lang="en">
 
@@ -116,3 +116,5 @@ module.exports = team => {
 </html>
     `;
 };
+
+export default render;
